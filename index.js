deb97caf793cac1986ece1993610a72b0cd2c57e
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2017-12-31 15:47:10 +0100

    fix: remove only file banner, do not remove jsdoc
    Close #1

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 // through2 is a thin wrapper around node transform streams
 var through = require('through2');
 var gutil = require('gulp-util');
-var stripComments = require('strip-comments').block;
+var stripBanner = require('strip-banner');
 var PluginError = gutil.PluginError;
 
 // consts
 const PLUGIN_NAME = 'gulp-strip-banner';
 
 function gulpStripBanner(){
-
-  var stream = through.obj(function(file, enc, cb){    
+  var stream = through.obj(function(file, enc, cb){
     if (file.isNull()) {
        // do nothing if no contents
     }
 
     if (file.isBuffer()) {
-      file.contents = new Buffer(stripComments(file.contents.toString(), {safe:true}));
+      file.contents = new Buffer(stripBanner(file.contents.toString()));
     }
 
     if (file.isStream()) {
--- test/main.js
+++ test/main.js
@@ -25,12 +25,74 @@ describe('strip_banner', function(){
 
   it('should work in buffer mode', function(done){
     fakeFile = getFakeFile('/** A banner  **/ Hello world /!** protected **/');
-    
+
     var stream = strip_banner();
     stream.on('data', function (newFile) {
       should.exist(newFile.contents);
       newFile.contents.toString().should.equal(' Hello world /!** protected **/');
     });
+    stream.once('end', done);
+    stream.write(fakeFile);
+    stream.end();
+  });
+
+  it('should not remove jsdoc', function(done) {
+    const banner = [
+      '/**',
+      ' * A banner',
+      ' */'
+    ].join('\n');
+
+    const body = [
+      '/**',
+      ' * A simple function.',
+      ' * @return {void}',
+      ' */',
+      'function() {',
+      '  console.log("Hello world");',
+      '}'
+    ].join('\n');
+
+    fakeFile = getFakeFile(banner + body);
+
+    const stream = strip_banner();
+
+    stream.on('data', function (newFile) {
+      should.exist(newFile.contents);
+      newFile.contents.toString().should.equal(body);
+    });
+
+    stream.once('end', done);
+    stream.write(fakeFile);
+    stream.end();
+  });
+
+  it('should remove banner prepended with space at the beginning', function(done) {
+    const banner = [
+      '/**',
+      ' * A banner',
+      ' */'
+    ].join('\n');
+
+    const body = [
+      '/**',
+      ' * A simple function.',
+      ' * @return {void}',
+      ' */',
+      'function() {',
+      '  console.log("Hello world");',
+      '}'
+    ].join('\n');
+
+    fakeFile = getFakeFile('\n' + banner + body);
+
+    const stream = strip_banner();
+
+    stream.on('data', function (newFile) {
+      should.exist(newFile.contents);
+      newFile.contents.toString().should.equal('\n' + body);
+    });
+
     stream.once('end', done);
     stream.write(fakeFile);
     stream.end();
